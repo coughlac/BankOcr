a79e1535a7a32a120fb57cfea61168e304cef411
Corina Usher <Corina.Usher@barclayscapital.com>
2015-01-01 21:50:19 +0000

    updated to read all digits in line for 27 chars and 9 digits

--- src/main/scala/Ocr.scala
+++ src/main/scala/Ocr.scala
@@ -1,14 +1,28 @@
 import scala.collection.immutable.IndexedSeq
-import scala.collection.immutable.Range.Inclusive
 
 object Ocr {
   def scan(input: String): String = {
     //need the first 3 chars of the first 3 lines
-    val lines: Array[String] = input.split('\n')
-    val line1: String = lines(0).take(3)
-    val line2: String = lines(1).take(3)
-    val line3: String = lines(2).take(3)
-    val accountNumberDigit: String = line1 + line2 + line3 match {
+    val lines = input.split('\n')
+
+    val digit: IndexedSeq[String] = for {
+      n <- 0 to 8
+      start <- 0 to 26 by 3
+    } yield parseDigit(lines, start)
+    digit.mkString
+  }
+
+  def parseDigit(lines: Array[String], start: Int): String = {
+    // val entryDigit: String = (start to start + 2).foldLeft("")((acc, index) => acc + lines(index).take(3))
+    val line1 = lines(0).substring(start, start + 3)
+    val line2 = lines(1).substring(start, start + 3)
+    val line3 = lines(2).substring(start, start + 3)
+    val entryDigit = line1 + line2 + line3
+    toDigit(entryDigit)
+  }
+
+  def toDigit(entry: String): String = {
+    entry match {
       case digit if digit == zero => "0"
       case digit if digit == one => "1"
       case digit if digit == two => "2"
@@ -20,7 +34,6 @@ object Ocr {
       case digit if digit == eight => "8"
       case digit if digit == nine => "9"
     }
-    accountNumberDigit * 9
   }
 
   val zero = "" +
